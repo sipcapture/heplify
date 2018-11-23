8cf80125427560b79a67ba3602d4e7e4f18d7843
Mauri de Souza Nunes <mauri870@gmail.com>
2018-11-23 21:10:14 -0200

    Add support for go modules, update Dockerfile

--- build_static.sh
+++ build_static.sh
@@ -4,24 +4,18 @@
 # https://www.moiji-mobile.com/2017/10/15/static-binaries-for-go-with-docker/
 
 # Make a static 64 bit binary:
-# docker run --rm=true -itv $PWD:/mnt alpine:3.8 /mnt/build_static.sh
+# docker run --rm=true -itv $PWD:/mnt golang:alpine /mnt/build_static.sh
 
 # Make a static 32 bit binary:
-# docker run --rm=true -itv $PWD:/mnt i386/alpine:3.8 /mnt/build_static.sh
+# docker run --rm=true -itv $PWD:/mnt i386/golang:alpine /mnt/build_static.sh
 
 set -ex
 
 apk update
-apk add linux-headers musl-dev gcc go libpcap-dev ca-certificates git
+apk add linux-headers musl-dev gcc libpcap-dev ca-certificates git
 
-mkdir /go
-export GOPATH=/go
-mkdir -p /go/src/github.com/negbie
-mkdir -p /mnt/out
-cp -a /mnt /go/src/github.com/negbie/heplify
-cd /go/src/github.com/negbie/heplify
+cd /mnt
 rm -f heplify*
-go get -v ./ ./
 go build --ldflags '-linkmode external -extldflags "-static -s -w"' -v ./
 ./heplify -rf example/pcap/rtp_rtcp_sip_ipv4_udp.pcap -rs -e -hs ""
 cp ./heplify /mnt/out/
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/sipcapture/heplify
+
+require (
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/coocood/freecache v1.0.1
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gogo/protobuf v1.1.1
+	github.com/google/gopacket v1.1.15
+	github.com/negbie/heplify v0.0.0-20181120122932-afa654d1922f
+	github.com/negbie/logp v0.0.0-20181018124411-1d803349a741
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.2.2
+	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a
+	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
+)
