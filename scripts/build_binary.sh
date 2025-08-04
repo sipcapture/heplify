d524943f75620a689a6e5f8ace6ccb36ac8372de
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2025-08-04 22:02:09 +0200

    Add auto-tidy step

--- scripts/build_binary.sh
+++ scripts/build_binary.sh
@@ -9,4 +9,4 @@ fi
 docker run --rm \
   -v $PWD:/app \
   golang:alpine \
-  sh -c "apk --update add linux-headers musl-dev gcc libpcap-dev ca-certificates git luajit-dev && cd /app && CGO_ENABLED=1 GOOS=linux go build -buildvcs=false -a --ldflags '-linkmode external -extldflags \"-static -s -w\"' -o heplify ."
+  sh -c "apk --update add linux-headers musl-dev gcc libpcap-dev ca-certificates git luajit-dev && cd /app && go mod tidy && CGO_ENABLED=1 GOOS=linux go build -buildvcs=false -a --ldflags '-linkmode external -extldflags \"-static -s -w\"' -o heplify ."
