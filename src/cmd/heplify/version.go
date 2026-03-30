ebd77a27a23f4a75d77aae9497ba38166ab3755a
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2026-03-30 18:24:10 +0200

    chore: bump version to 2.0.4

--- src/cmd/heplify/version.go
+++ src/cmd/heplify/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Version is set at build time via -X main.Version=<ver> ldflags.
-var Version = "2.0.3"
+var Version = "2.0.4"
 
 // BuildDate is set at build time via -X main.BuildDate=<date> ldflags.
 var BuildDate = "unknown"
